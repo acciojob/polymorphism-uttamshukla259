d10f2f3bc698572b05f772114dffc3de1b6e448a
uttamshukla259 <uttamshukla259@gmail.com>
2023-01-13 12:44:30 +0530

    code 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,23 @@ package com.driver;
 
 public class Main {
 
+    public static class Product {
+
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(5,6);
+        p.product(2,4,6);
+        p.product(5.5,2.2);
+    }
 }
